48ddd4f0535f8e9a28ec29e39ccb52d73ac85341
duva-ha <dvhai.vi@gmail.com>
2026-05-10 13:22:25 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const lessonData = {
+    "k10b1": {
+        title: "BÀI 1: CÔNG NGHỆ VÀ ĐỜI SỐNG",
+        slides: [
+            { h: "Khái niệm Công nghệ", p: "Công nghệ là giải pháp...", img: "url_anh.jpg" },
+            { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
+        ]
+    }
+};
