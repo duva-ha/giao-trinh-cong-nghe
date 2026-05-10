c70799d6924bd9afa85856c882593bf72b50b6a8
duva-ha <dvhai.vi@gmail.com>
2026-05-10 13:35:50 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,12 +3,12 @@ const lessonData = {
         title: "BÀI 1: CÔNG NGHỆ VÀ ĐỜI SỐNG",
         slides: [
             { h: "Khái niệm Công nghệ", p: "Công nghệ là giải pháp...", img: "url_anh.jpg" },
-            { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
-             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
-             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
-             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
-             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
-             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
+            { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." },
+             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." },
+             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." },
+             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." },
+             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." },
+             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." },
              { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
         ]
     }
