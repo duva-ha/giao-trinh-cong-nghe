23214603e2aecfa4d0ea0fb0ebf52cc16d3f54b8
duva-ha <dvhai.vi@gmail.com>
2026-05-10 13:28:18 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const lessonData = {
     "k10b1": {
         title: "BÀI 1: CÔNG NGHỆ VÀ ĐỜI SỐNG",
         slides: [
-            { h: "Khái niệm Công nghệ", p: "Công nghệ là giải pháp...", img: "url_anh.jpg" },
+            { h: "Khái quát về khoa học, kĩ thuật, công nghệ", p: "Khoa học: Là hệ thống tri thức về mọi quy luật và sự vận động của vật chất, nhằm nhận thức, mô tả, giải thích và tiên đoán về các sự vật, hiện tượng tự nhiên dựa trên bằng chứng từ quan sát và thực nghiệm.", p: "Kĩ thuật: Là việc ứng dụng các nguyên lí khoa học vào việc thiết kế, chế tạo, vận hành các máy móc, thiết bị, công trình... một cách hiệu quả và kinh tế nhất.", img: "url_anh.jpg" },
             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
         ]
     }
