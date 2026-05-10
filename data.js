e9dd033db508575f545825faa1163271e0d868f3
duva-ha <dvhai.vi@gmail.com>
2026-05-10 13:31:32 +0700

    Update data.js

--- data.js
+++ data.js
@@ -4,6 +4,8 @@ const lessonData = {
         slides: [
             { h: "Khái quát về khoa học, kĩ thuật, công nghệ", p: "Khoa học: Là hệ thống tri thức về mọi quy luật và sự vận động của vật chất, nhằm nhận thức, mô tả, giải thích và tiên đoán về các sự vật, hiện tượng tự nhiên dựa trên bằng chứng từ quan sát và thực nghiệm.", p: "Kĩ thuật: Là việc ứng dụng các nguyên lí khoa học vào việc thiết kế, chế tạo, vận hành các máy móc, thiết bị, công trình... một cách hiệu quả và kinh tế nhất.", img: "url_anh.jpg" },
             { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
+            { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
+            { h: "Vai trò", p: "Cải thiện chất lượng cuộc sống..." }
         ]
     }
 };
